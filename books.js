82dde818ca7a5817caa9b6201023c3c59aab7668
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-01-07 21:56:29 -0800

    css

--- books.js
+++ books.js
@@ -1,5 +1,5 @@
 
-const myLibrary = []
+let myLibrary = []
 
 function Book(title, author, pages, read) {
     //constructor
@@ -10,11 +10,26 @@ function Book(title, author, pages, read) {
     this.read = Boolean(read);
 };
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
     renderLibrary();
 };
+
+function removeBookById(id) {
+    myLibrary = myLibrary.filter((book) => book.id !== id)
+}
+
+function toggleReadById(id) {
+    const book = myLibrary.find((book) => book.id === id)
+    if (book) {
+        book.read = !book.read;
+    }
+}
 
 function renderLibrary() {
     const library = document.querySelector("#library");
@@ -44,6 +59,25 @@ function renderLibrary() {
         const actions = document.createElement("div");
         actions.classList.add("book-actions");
 
+        const toggleBtn = document.createElement("button");
+        toggleBtn.textContent = "Toggle Read";
+        toggleBtn.dataset.id = book.id;
+
+        toggleBtn.addEventListener("click", () => {
+            book.toggleRead();
+            renderLibrary();
+        });
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.dataset.id = book.id;
+
+        removeBtn.addEventListener("click", () => {
+            removeBookById(book.id);
+            renderLibrary();
+        });
+
+        actions.append(toggleBtn, removeBtn);
         card.append(titleEl, authorEl, pagesEl, readEl, actions);
         library.appendChild(card);
     });
@@ -51,3 +85,37 @@ function renderLibrary() {
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("Clean Code", "Robert C. Martin", 464, false);
+
+const newBookBtn = document.querySelector("#newBookBtn");
+const dialog = document.querySelector("#bookDialog");
+const form = document.querySelector("#bookForm");
+
+const cancelBtn = document.querySelector("#cancelBtn");
+const closeDialogBtn = document.querySelector("#closeDialogBtn");
+
+newBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = event.target.title.value.trim();
+    const author = event.target.author.value.trim();
+    const pages = Number(event.target.pages.value);
+    const read = event.target.read.checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    form.reset();
+    dialog.close();
+});
+
+cancelBtn.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+});
+
+closeDialogBtn.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+})
